22ca35b0652af95d7ab2d8241ee4f3a1637f8e6b
adamz0210 <64041152+adamz0210@users.noreply.github.com>
2020-11-23 14:06:42 -0500

    Variable Fix

--- Variable/obix-niagara-variable.js
+++ Variable/obix-niagara-variable.js
@@ -1,118 +1,122 @@
-module.exports = function(RED) {
-    function VariableNode(config) {
-        RED.nodes.createNode(this,config);
-        const node = this;
-        var context = this.context();
-        const axios = require("axios");
-        const convert = require('xml-js');
-        const https = require('https');
+module.exports = function (RED) {
 
-        // Connecting Configuration Node
-        node.serverConfig = RED.nodes.getNode(config.serverConfig);
-        
-        this.on('input', function(msg, send, done) {
+    const axios = require("axios");
+    const convert = require('xml-js');
+    const https = require('https');
+    var tcpp = require('tcp-ping');
 
-            if(node.serverConfig){
+    function throwError(node, msg, err, status) {
+        node.status({ fill: "red", shape: "dot", text: status });
+        node.error(err, msg);
+    }
 
-                // Setting all variables if passed in, if not, we will use the preset values
-                context.set('username', msg.username || node.serverConfig.username);
-                context.set('password', msg.password || node.serverConfig.password);
-                context.set('ipAddress', msg.ipAddress || node.serverConfig.host);
-                context.set('httpsPort', msg.httpsPort || node.serverConfig.port);
-                context.set('path', msg.path || config.path);
-                context.set('action', msg.method || config.action);
-                context.set('value', msg.value || config.value);
+    function onInput(node, config, msg) {
+        // Variables
+        try {
+            // Setting all variables if passed in, if not, we will use the preset values
+            var username = msg.username || node.serverConfig.username;
+            var password = msg.password || node.serverConfig.password;
+            var ipAddress = msg.ipAddress || node.serverConfig.host;
+            var httpsPort = msg.httpsPort || node.serverConfig.port;
+            var path = msg.path || config.path;
+            var action = msg.method || config.action;
+            var value = msg.value || config.value;
 
-                // If missing a configuration variable, return error
-                if(!context.get('username')){ throwError(msg, "Invalid Parameters : Missing Obix Username", "red", "ring", "Missing Username"); return; }
-                if(!context.get('password')){ throwError(msg, "Invalid Parameters : Missing Obix Password", "red", "ring", "Missing Password"); return; }
-                if(!context.get('ipAddress')){ throwError(msg, "Invalid Parameters : Missing Niagara IP Address", "red", "ring", "Missing IP Address"); return; }
-                if(!context.get('httpsPort')){ throwError(msg, "Invalid Parameters : Missing Niagara HTTPS Port", "red", "ring", "Missing HTTPS Port"); return; }
-                if(!context.get('path')){ throwError(msg, "Invalid Parameters : Missing Variable Path", "red", "ring", "Missing Variable Path"); return; }
-                if((!context.get('value')) && (context.get('action') == "POST")){ throwError(msg, "Invalid Parameters : Missing Write Value", "red", "ring", "Missing Write Value"); return; }
-                
-                // Slice '/' from the path if it exists
-                context.get('path').charAt(context.get('path').length - 1) == '/' ? context.set('path', context.get('path').slice(0, -1)) : null;
-                context.get('path').charAt(0) == '/' ? context.set('path', context.get('path').slice(1)) : null;
+            // If missing a configuration variable, return error
+            if (!username) { throw "Missing Username"; }
+            if (!password) { throw "Missing Password"; }
+            if (!ipAddress) { throw "Missing IP Address"; }
+            if (!httpsPort) { throw "Missing HTTPS Port"; }
+            if (!path) { throw "Missing Variable Path"; }
+            if ((value) && (action == "POST")) { throw "Missing Write Value"; }
 
-                // Set Fetch parameters
-                if(context.get('action') == "POST"){
-                    var apiCallConfig = {
-                        method: 'post',
-                        url: "https://" + context.get('ipAddress') + ":" + context.get('httpsPort') + "/obix/config/" + context.get('path') + "/set/",
-                        auth: {
-                            username: context.get('username'), 
-                            password: context.get('password')
-                        },
-                        httpsAgent: new https.Agent({ rejectUnauthorized: false }),
-                        data : '<real val="' + context.get('value') + '"/>'
-                    };
-                }else{
-                    var apiCallConfig = {
-                        method: 'get',
-                        url: "https://" + context.get('ipAddress') + ":" + context.get('httpsPort') + "/obix/config/" + context.get('path') + "/out/",
-                        auth: {
-                            username: context.get('username'), 
-                            password: context.get('password')
-                        },
-                        httpsAgent: new https.Agent({ rejectUnauthorized: false }),
-                    };
+            // Slice '/' from the path if it exists
+            path.charAt(path.length - 1) == '/' ? path = path.slice(0, -1) : null;
+            path.charAt(0) == '/' ? path = path.slice(1) : null;
+
+        } catch (error) {
+            throwError(node, msg, error, error);
+            return;
+        }
+
+        tcpp.ping({ "address": ipAddress, "port": Number(httpsPort), "timeout": 500, "attempts": 1 }, async function (err, data) {
+            if (err) { throwError(node, msg, "Error in TCP Ping: " + err, "Error in TCP Ping"); return; }
+            if (data.results[0].err) { throwError(node, msg, "TCP Ping Failed: Host/Port Invalid", "Host/Port Unavailable"); return; }
+
+            // Success Connection
+            // Set Fetch parameters
+            if (action == "POST") {
+                var apiCallConfig = {
+                    method: 'post',
+                    url: "https://" + ipAddress + ":" + httpsPort + "/obix/config/" + path + "/set/",
+                    auth: {
+                        username: username,
+                        password: password
+                    },
+                    httpsAgent: new https.Agent({ rejectUnauthorized: false }),
+                    data: '<real val="' + value + '"/>'
+                };
+            } else {
+                var apiCallConfig = {
+                    method: 'get',
+                    url: "https://" + ipAddress + ":" + httpsPort + "/obix/config/" + path + "/out/",
+                    auth: {
+                        username: username,
+                        password: password
+                    },
+                    httpsAgent: new https.Agent({ rejectUnauthorized: false }),
+                };
+            }
+
+            try {
+                const response = await axios(apiCallConfig);
+                const data = convert.xml2js(response.data, { compact: true, spaces: 4 });
+
+                if (data.err) {
+                    data.err._attributes.is == "obix:BadUriErr" ? status = "Invalid Variable Path" : status = "Unknown Error";
+                    data.err._attributes.display.includes("Invalid") ? status = "Invalid Data Type" : null;
+                    throwError(node, msg, "Error: " + status, status);
+                    return;
                 }
 
-                axios(apiCallConfig)
-                .then(function (response) {
-                    // Convert Response to JSON
-                    var data = convert.xml2js(response.data, {compact: true, spaces: 4});
-
-                    if(data.err){
-                        data.err._attributes.is == "obix:BadUriErr" ? status = "Invalid Variable Path" : status = "Unknown Error";
-                        data.err._attributes.display.includes("Invalid") ? status = "Invalid Data Type" : null;
-                        throwError(msg, "Error in Preset Query Search: " + status, "red", "dot", status);
-                        return;
-                    }
-
-                    // After the Request is made, and the JSON data is returned...
-                    try{     
-                        if(data.enum){ context.set('value', data.enum._attributes.val) }
-                        else if(data.bool){ context.set('value', data.bool._attributes.val) }
-                        else if(data.str){ context.set('value', data.str._attributes.val) }
-                        else if(data.real){ context.set('value', data.real._attributes.val) }
-                        else{ throwError(msg, "Error with Variable Parsing, Can't Find Data Type", "red", "dot", "Error with Variable Parsing"); return; }
-                        msg.payload = {
-                            "Variable": context.get('path'),
-                            "Value": context.get('value'),
-                        };
-                    }catch(error){
-                        throwError(msg, "Error with Variable Parsing: " + error, "red", "dot", "Error with Variable Parsing");
-                        return;
-                    }
-
-                    node.status({fill:"green",shape:"dot",text:"Success"});
-                    node.send(msg);
-
-                }).catch(function (error) {
-                    if(String(error).includes("404")){throwError(msg, "Error Invalid IP/Port: " + error, "red", "dot", "Invalid IP/Port"); return;}
-                    if(String(error).includes("401")){throwError(msg, "Error Invalid Credentials: " + error, "red", "dot", "Invalid Credentials"); return;}
-                    throwError(msg, "Error with Variable Fetch: " + error, "red", "dot", "Error with Variable Fetch");
+                // After the Request is made, and the JSON data is returned...
+                try {
+                    var result;
+                    if (data.enum) { result = data.enum._attributes.val }
+                    else if (data.bool) { result = data.bool._attributes.val }
+                    else if (data.str) { result = data.str._attributes.val }
+                    else if (data.real) { result = data.real._attributes.val }
+                    else { throw "Error with Variable Parsing: Can't Find Data Type"; }
+                    msg.payload = {
+                        "Variable": path,
+                        "Value": result,
+                    };
+                } catch (error) {
+                    throwError(node, msg, error, error);
                     return;
-                })
-            }
-            else{
-                throwError(msg, "No Config Node Set (If Passing in config variables from msg, Configure a blank config node)", "red", "ring", "No Config Set");
+                }
+
+                node.status({ fill: "green", shape: "dot", text: "Success" });
+                node.send(msg);
+            } catch (error) {
+                throwError(node, msg, error, error);
                 return;
             }
-            
-            if (done) {
-                done();
-            }
         });
-        
-        function throwError(msg, err, color, shape, status){
-            node.error(err, msg);
-            node.status({fill: color, shape: shape, text: status});
-            msg.payload = err;
-            node.send(msg);
-        }
+
+    }
+
+    function VariableNode(config) {
+
+        RED.nodes.createNode(this, config);
+
+        var node = this;
+        node.serverConfig = RED.nodes.getNode(config.serverConfig);
+
+        this.on('input', async function (msg) {
+            onInput(node, config, msg);
+        });
+        node.status({ fill: "blue", shape: "dot", text: "Ready" });
     }
     RED.nodes.registerType("Niagara Obix Variable", VariableNode);
 }
